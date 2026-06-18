e974fb8cf2a1b5ee534d765ba11a03f7111c32ae
SebXpress <benalcazarsebas8173@proton.me>
2026-06-18 12:36:15 -0500

    feat: simular endpoints independientes para service-alpha y service-beta

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,5 +11,32 @@ export class AuthController {
         // 3. Si son válidas, crear un payload y llamar a JwtService.signToken(payload).
         // 4. Responder con el token generado.
         // 5. Si son inválidas, responder con status 401.
+    
+        //obtenemos el usuario y contraseña de la peticion
+        const {user, password} = req.body;
+
+        // simulamos que verificamos las credenciales en una base de datos
+        if(username === 'admin' && password === 'secure1230'){
+
+            //creamos un usuario de prueba para inyectarlo en el payload
+            const mockUser = {id:'usr_998822', name: 'Administrador del sistema'};
+
+            //llamamos a nuestro servicio para firmar y crear el token
+            const token = JwtService.signToken(mockUser);
+
+            // devolvemos el token al cliente con status 200 de exito
+            return res.status(200).json({
+                message: 'autenticacion existosa',
+                access_token: token,
+                token_type: 'Bearer',
+                expires_in: '60s'
+            });
+        }
+
+        // si los datos ingresados son incorrectos devolvemos un error tipo 401
+        return res.status(401).json({
+            erro: 'Unauthorized',
+            message: 'Credenciales invalidas'
+        });
     }
 }
--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -6,6 +6,16 @@ export class ResourceController {
         // TODO: Implementar respuesta para Microservicio Alpha.
         // 1. Responder con un mensaje que indique acceso exitoso al Servicio Alpha.
         // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        
+        //respondemos con exito indicando que se entro al microservicio alfa
+        return res.status(200).json({
+            service: 'Microservicio Alpha',
+            status: 'Authorized',
+            message: 'Conexion exitosa a la zona segura de alpha de forma stateless',
+
+            // enviamos los datos del usuario extraidos del token por el middleware
+            user: req.user
+        });
     }
 
     /**
@@ -15,5 +25,12 @@ export class ResourceController {
         // TODO: Implementar respuesta para Microservicio Beta.
         // 1. Responder con un mensaje que indique acceso exitoso al Servicio Beta.
         // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        
+        //respondemos con exito indicando que se entro al microservicio beta
+        return res.status(200).json({
+            service: 'Microservicio Beta',
+            status: 'Authorized',
+            message: 'Conexion exitosa a la zona segura de beta utilizando llave publica'
+        });
     }
 }
