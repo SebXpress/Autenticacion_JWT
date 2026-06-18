12a14fcd26351a36d8c33077611cc7584f53cbed
SebXpress <benalcazarsebas8173@proton.me>
2026-06-18 10:42:34 -0500

    feat: implementar middleware de verificacion automatica y control de expiracion

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,4 +7,43 @@ export const authMiddleware = (req, res, next) => {
     // 3. Llamar a JwtService.verifyToken(token).
     // 4. Si el token es válido, adjuntar el payload a req.user y llamar a next().
     // 5. Si el token es inválido, responder con status 403.
+
+    // extraemos la cabecera de autorizacion de la peticion http
+    const autHeader = req.headers['authorization'];
+
+    // si no existe la cabecera o no tiene la palabra, bloqueamos el acceso
+    if(!autHeader || !autHeader.startsWith('Bearer')){
+        return res.satatus(401).json({
+            error: 'Unauthorized',
+            message: 'acceso denegado, ausencia de bearer token en las cabeceras '
+        });
+    }
+
+    // separamos la palabra bearer del token real usando un espacio
+    const token = autHeader.split(' ')[1];
+
+    try{
+        //decodificamos y validamos la firma del toke
+        const decoded = JwtService.verifyToken(token);
+
+        // si es valido vamos a guardar los datos del usuario en la peticion actual
+        req.user = decoded;
+
+        //permitimos que la peticion continue hacia el controlador de la ruta
+        next();
+    }catch (error){
+        // comprobamos si el error es por el tiempo de expiracion
+        if(error.name === 'TokenExpiredError'){
+            return res.satatus(401).json({
+                error: 'TokenExpiredError',
+                message: 'El token ha expirado, supero el limite de 1 minuto esperado',
+            });
+        }
+
+        // si es clualquier otro error significa que el token fue manipulado
+        return res.status(403).json({
+            error: 'Forbidden',
+            message: 'Token invalido o firma digital corrupta',
+        });
+    }
 };
